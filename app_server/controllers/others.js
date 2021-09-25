44d0d8691f6e7b11647839df41d42ea9a3909bc1
97Kzone <mmnm3@naver.com>
2021-09-25 17:17:02 +0900

    5-6 commit

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,5 @@
 const about = (req, res) => {
-    res.render('index', {title : 'About by 2016250002 강동표' })
+    res.render('generic-text', {title : 'About by 2016250002 강동표' })
 };
 
 module.exports = {
